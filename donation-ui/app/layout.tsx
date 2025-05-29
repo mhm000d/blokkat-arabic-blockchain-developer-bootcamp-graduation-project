1b4ff8128a0552b253a4e2d5648af8add2799dd7
Mahmoud <mahmoudjamal01m@gmail.com>
2025-05-29 11:07:24 +0300

    tracking visitors and page views by import vercel analytics

--- donation-ui/app/layout.tsx
+++ donation-ui/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
@@ -26,6 +27,7 @@ export default async function RootLayout({
         <html lang="en">
         <body className={inter.className}>
         <ContextProvider cookies={cookies}>{children}</ContextProvider>
+        <Analytics />
         <SpeedInsights/>
         </body>
         </html>
