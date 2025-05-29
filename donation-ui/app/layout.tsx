4e92b0e5d581f1adc8515c13fbd369f145b8838e
Mahmoud <mahmoudjamal01m@gmail.com>
2025-05-29 11:02:25 +0300

    tracking performance metrics by import vercel seed-insights

--- donation-ui/app/layout.tsx
+++ donation-ui/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +26,7 @@ export default async function RootLayout({
         <html lang="en">
         <body className={inter.className}>
         <ContextProvider cookies={cookies}>{children}</ContextProvider>
+        <SpeedInsights/>
         </body>
         </html>
     )
